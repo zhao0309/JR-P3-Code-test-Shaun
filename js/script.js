d34adcdbe95d087dcf6ecb8d0cb39d43782a48f8
Sue Huang <huchshm06@gmail.com>
2022-08-29 14:43:46 +0930

    update comment

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 for(let i=1; i<101; i++) {
     if(i%3===0 && i%5===0) {
-        console.log('FizzBuzz'); // use continue; to break one iteration if not allowed
+        console.log('FizzBuzz'); 
     }
     else if(i%3===0) {
         console.log('Fizz');}
