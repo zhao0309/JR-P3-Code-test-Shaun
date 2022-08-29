3ef4709532950663595757a6aeb543823a045ab9
Sue Huang <huchshm06@gmail.com>
2022-08-29 13:59:00 +0930

    FizzBuzz

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 // write your code here
 
-
+for(let i=1; i<101; i++) {
+    if(i%15===0) {
+        console.log('FizzBuzz'); // use continue; to break one iteration if not allowed
+    }
+    else if(i%3===0) {
+        console.log('Fizz');}
+    else if(i%5===0) {
+        console.log('Buzz');}
+    else console.log(i);
+}
