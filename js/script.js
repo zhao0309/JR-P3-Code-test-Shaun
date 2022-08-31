ea5fde2796397073dced43efb58bba4eb0adde7d
Chen Zhao <joe.zhao0309@gmail.com>
2022-08-31 15:30:24 +1000

    Github test

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 // write your code here
-
-for(let i=1; i<101; i++) {
-    if(i%3===0 && i%5===0) {
-        console.log('FizzBuzz'); 
+//Github push test 
+for (var i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log('FizzBuzz');
     }
-    else if(i%3===0) {
-        console.log('Fizz');}
-    else if(i%5===0) {
-        console.log('Buzz');}
-    else console.log(i);
+    else if (i % 3 == 0) {
+        console.log('Fizz');
+    }
+    else if (i % 5 == 0) {
+        console.log('Buzz');
+    } else {
+        console.log(i);
+    }
 }
